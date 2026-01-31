d3deb2dd2011839d3ef9b8babe3a78f35f0cfcf0
sanjeev <Sanjeevsanju4000@gmail.com>
2026-01-31 10:45:27 +0530

    practice code 14

---
+++ basics/practice_14.py
@@ -0,0 +1,8 @@
+s_percentage = int(input("Enter your Percentage : "))
+if(s_percentage>=70):
+    name = input("Enter your name : ")
+    department =input("Enter your department : ")
+    location =input("Enter you location : ")
+    print("you are eligible : ",name)
+else:
+    print("You are not eligible")
