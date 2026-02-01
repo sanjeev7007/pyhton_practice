8c5da7dc6a62a5eb5bfd7c2928b78874149df6f2
sanjeev <Sanjeevsanju4000@gmail.com>
2026-02-01 16:21:18 +0530

    uploaded ex1 to ex4

---
+++ hackerrank/ex1.py
@@ -0,0 +1,10 @@
+n = int(input())
+if(n%2!=0):
+        print("Weird")
+if(n%2==0):
+        if(5>n>2):
+                print("Not Weird")
+        elif(20>=n>6):
+                print("Weird")
+        elif(n>22):
+                print("Not Weird")
