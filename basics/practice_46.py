710a5161b5f67289824ceddf5263d233704b9476
sanjeev <Sanjeevsanju4000@gmail.com>
2026-02-04 15:51:30 +0530

    practice 46

---
+++ basics/practice_46.py
@@ -0,0 +1,13 @@
+n=int(input())
+if n<=1:
+    print("Not Prime")
+else:
+    for i in range (2,n):
+        if n%i==0:
+            print("Not Prime")
+            break
+    else:
+        print("prime")
+
+
+    
